50d88678f4b8e0ed9f85d4719cc0f1522fc2d22b
Lalit Kapoor <lalitkapoor@gmail.com>
2012-02-29 06:31:06 -0600

    initial commit

---
+++ www/static/js/master.js
@@ -0,0 +1,101 @@
+
+/*api.auth.facebook = function(accessToken, callback){
+  api._post("http://www.goodybag.com/api/consumers/fblogin", {accessToken:accessToken}, callback);
+};
+
+api.auth.session = function (update, callback){ //update param triggers session update from db
+  if(Object.isFunction(update)){
+    callback = update;
+    params = "";
+  }
+  else{
+    params = "?updateSession=1";
+  }
+  api._get("http://www.goodybag.com/api/consumers/session"+params, callback);
+};
+*/
+
+if (typeof PhoneGap == 'undefined') console.log('PhoneGap variable does not exist. Check that you have included phonegap.js correctly');
+if (typeof PG == 'undefined') console.log('PG variable does not exist. Check that you have included pg-plugin-fb-connect.js correctly');
+if (typeof FB == 'undefined') console.log('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+FB.Event.subscribe('auth.login', function(response) {
+  console.log('auth.login event');
+});
+
+FB.Event.subscribe('auth.logout', function(response) {
+  console.log('auth.logout event');
+});
+
+FB.Event.subscribe('auth.sessionChange', function(response) {
+  console.log('auth.sessionChange event');
+});
+
+FB.Event.subscribe('auth.statusChange', function(response) {
+  console.log('auth.statusChange event');
+});
+
+function getSession() {
+  alert("session: " + JSON.stringify(FB.getSession()));
+}
+
+function getLoginStatus() {
+  FB.getLoginStatus(function(response) {
+    if (response.status == 'connected') {
+      console.log('logged in');
+    } else {
+      console.log('not logged in');
+    }
+  });
+}
+
+function me() {
+  FB.api('/me/friends', function(response) {
+    if (response.error) {
+      alert(JSON.stringify(response.error));
+    } else {
+      var data = document.getElementById('data');
+      response.data.forEach(function(item) {
+        var d = document.createElement('div');
+        d.innerHTML = item.name;
+        data.appendChild(d);
+      });
+    }
+  });
+}
+
+function logout() {
+  FB.logout(function(response) {
+    console.log('logged out');
+  });
+}
+
+function login() {
+  console.log("Attempting to login with facebook");
+  FB.login(function(response) {
+    if (response.session) {
+      console.log("LOGGED IN");
+      console.log(JSON.stringify(response));
+      var accessToken = response.session.access_token;
+      console.log(accessToken);
+      FB.api('/me', function(response) {
+        api.auth.facebook(accessToken,function(error,consumer){
+          if(error != null){
+            alert(error.message);
+            return;
+          } else {
+            console.log("logged in to goodybag");
+            api.auth.session(function(error, data){
+              $("#qrcode").qrcode({text:data.barcodeId, render:"table", width:200, height:200 });
+              document.location.href="#barcode-page";
+            });
+          }
+        })
+      });
+    } else {
+      console.log('not logged in');
+    }
+  }, {
+    perms: "email"
+  });
+}
