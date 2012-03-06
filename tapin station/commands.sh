35ccfdd6c1ed4562c2e156066613f356a57781bb
Lalit Kapoor <lalitkapoor@gmail.com>
2012-03-05 22:56:50 -0600

    updating command to install Goodybag TapIn Station

--- tapin station/commands.sh
+++ tapin station/commands.sh
@@ -14,4 +14,4 @@
 ./adb remount; ./adb shell mv /system/app/SystemUI.odex /system/app/SystemUI.odexold; ./adb shell mv /system/app/SystemUI.apk /system/app/SystemUI.apkold; ./adb reboot
 
 #Uninstall Goodybag TapIn Station, Launch Settings App, Install Goodybag TapIn Station, Launch Goodybag TapIn Station
-./adb uninstall com.google.zxing.client.android; ./adb shell am start -a android.intent.action.MAIN -n com.android.settings/.Settings; ./adb install /goodybag/mobile/tapin\ station/android/android/bin/SettingActivity.apk; ./adb shell am start -a android.intent.action.MAIN -n com.google.zxing.client.android/.tapin.activities.SettingActivity
+./adb uninstall com.google.zxing.client.android; ./adb shell am start -a android.intent.action.MAIN -n com.android.settings/.Settings; ./adb install /goodybag/mobile/tapin\ station/android/android/bin/Goodybag\ TapIn\ Station.apk; ./adb shell am start -a android.intent.action.MAIN -n com.google.zxing.client.android/com.goodybag.tapin.station.activities.SettingActivity
