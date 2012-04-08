1fa7d88a19c7f33830e3530d6801ec2067bbd904
Lalit Kapoor <lalitkapoor@gmail.com>
2012-04-08 05:35:57 -0500

    testing better detection of iPhone/iPad in app vs Safari

--- apps/iphone/Goodybag/Goodybag/Classes/AppDelegate.m
+++ apps/iphone/Goodybag/Goodybag/Classes/AppDelegate.m
@@ -61,6 +61,7 @@
  */
 - (BOOL) application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {    
+    [NSClassFromString(@"WebView") _enableRemoteInspector];
 
     NSURL* url = [launchOptions objectForKey:UIApplicationLaunchOptionsURLKey];
     if (url && [url isKindOfClass:[NSURL class]]) {
