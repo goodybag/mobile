63489be3fc705af8291f203733b83e07e1262a2c
John Fawcett <jrf0110@gmail.com>
2012-05-31 13:42:28 -0500

    WIP

--- www/static/js/config.js
+++ www/static/js/config.js
@@ -16,6 +16,7 @@
       changeHash: false
     },
     theme: 'b'
+  , consumerPicture: "https://s3.amazonaws.com/goodybag-uploads/consumers/{{id}}-85.png"
   , iosLt5: (function(){
       return (/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) && (/OS [1-4]_._. like Mac OS X/i.test(navigator.userAgent));
     })()
