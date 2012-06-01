fb134b4614e2ee9ecb92946e033751c78d9a8363
John Fawcett <jrf0110@gmail.com>
2012-06-01 12:01:02 -0500

    Moved templates and fragments to index.html

--- www/static/js/templates.js
+++ www/static/js/templates.js
@@ -53,25 +53,22 @@
   templates.tapIn = "tap-in-tmpl";
 
   this.app.compileTemplates = function(callback){
-    $.get('/partials.html?rand='+ Math.floor(Math.random()*1000), function(content, status){
-      $('body').append(content);
-      var compile = function(obj){
-        var compiled = {};
-        for (var key in obj){
-          if (typeof obj[key] !== "string"){
-            compiled[key] = compile(obj[key]);
-          }else {
-            compiled[key] = Handlebars.compile($("#" + obj[key]).html());
-          }
+    var compile = function(obj){
+      var compiled = {};
+      for (var key in obj){
+        if (typeof obj[key] !== "string"){
+          compiled[key] = compile(obj[key]);
+        }else {
+          compiled[key] = Handlebars.compile($("#" + obj[key]).html());
         }
-        return compiled;
-      };
-      // Export
-      app.templates = compile(templates);
-      app.fragments = compile(fragments);
+      }
+      return compiled;
+    };
+    // Export
+    app.templates = compile(templates);
+    app.fragments = compile(fragments);
 
-      app.trigger("templates:compiled");
-      if (utils.exists(callback)) callback();
-    });
+    app.trigger("templates:compiled");
+    if (utils.exists(callback)) callback();
   };
 }).call(this);
