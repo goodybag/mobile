e5fbf1ac8c89cebc91ad1cf049d3ff96caf3675f
John Fawcett <jrf0110@gmail.com>
2012-05-31 21:29:56 -0500

    fixed accidental save

--- www/static/js/views.js
+++ www/static/js/views.js
@@ -67,13 +67,31 @@
     }
   });
 
+  views.GoodiesComingSoon = utils.View.extend({
+    className: 'page goodies-coming-soon'
+  , render: function(){
+      this.el.innerHTML = app.templates.goodiesComingSoon();
+      return this;
+    }
+  });
+
+  // Now incomplete until we fix API shtuff
   views.Goodies = utils.View.extend({
     className: 'page goodies',
-    initialize: function(){
+    initialize: function(options){
+      this.goodies = options.goodies;
       return this;
     },
     render: function(){
       $(this.el).html(app.templates.goodies());
+      var $el = $(this.el).find('#goodies-list');
+      for (var i = 0; i < this.goodies.length; i++){
+        $el.append(
+          new app.Views.Goody({
+            json: this.goodies[i]
+          }).render().el
+        );
+      }
       return this;
     },
     addGoody: function(goody){
@@ -85,11 +103,12 @@
 
   views.Goody = utils.View.extend({
     className: 'goody',
-    initialize: function(){
+    initialize: function(options){
+      this.json = options.json;
       return this;
     },
     render: function(){
-      $(this.el).html(app.fragments.goody(this.model.toJSON()));
+      $(this.el).html(app.fragments.goody(this.json));
       return this;
     }
   });
@@ -531,12 +550,10 @@
   });
 
   views.Place = utils.View.extend({
-    className: 'inline-columns place'
+    className: 'inline-columns place push-link'
     , events: {
-      'click .view-details': 'viewDetails'
-    }
-    , initialize: function(){
-    }
+        'click': 'viewDetails'
+      }
     , render: function() {
       $(this.el).html(app.fragments.place(this.model.toJSON()));
       return this;
@@ -544,8 +561,6 @@
     , viewDetails: function(){
       /* display business details */
       var self = this;
-      console.log("View Details");
-      console.log(self.model);
       app.changePage(function(done){
         api.businesses.getOneEquipped(self.model.get('_id'), function(error, business){
           if(utils.exists(error)){
