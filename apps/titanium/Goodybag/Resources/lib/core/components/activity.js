f327abb17e588c4eef62eaefaf17d5e7af4fe2da
John Fawcett <jrf0110@gmail.com>
2012-08-13 13:47:25 -0500

    WIP

--- apps/titanium/Goodybag/Resources/lib/core/components/activity.js
+++ apps/titanium/Goodybag/Resources/lib/core/components/activity.js
@@ -7,21 +7,42 @@
   
   var constructor = function(model){
     this.model = model;
-    
-    // Main view wrapper
-    this.base = Titanium.UI.createView({
-      width: Ti.UI.FILL
-    , height: Ti.UI.SIZE
-    , top: '6dp'
-    , backgroundColor: '#fff'
-    , borderColor: '#ccc'
-    , borderWidth: 1
-    , borderRadius: 5
-    , layout: 'horizontal'
-    });
+    this.setViews();
   };
   constructor.prototype = {
-    
+    setViews: function(){
+      this.views = {
+      // Main view wrapper
+        base: $ui.createView({
+          width: Ti.UI.FILL
+        , height: Ti.UI.SIZE
+        , top: '6dp'
+        , backgroundColor: '#fff'
+        , borderColor: '#ccc'
+        , borderWidth: 1
+        , borderRadius: 5
+        , layout: 'horizontal'
+        })
+        // Profile picture
+      , image: createImageView({
+          image: "https://s3.amazonaws.com/goodybag-uploads/consumers" + imgSrc + "-85.png"
+        , title: attr.who.screenName
+        , width: "42dp"
+        , height: "42dp"
+        // For now, I'm getting rid of border due to the uncertainty of whether or not
+        // A user even has a profile picture
+        // , borderWidth: 1
+        // , borderColor: isUnknown ? '#ededed' : '#ccc'
+        , borderRadius: 5
+        , top: '4dp'
+        , left: "-3dp"
+        })
+        
+      , rightSide: {
+          
+        }
+      };
+    }
   };
   
   GB.Views.Activity = constructor;
