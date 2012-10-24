16e5c23bcba6086dc42affbebb0e684170b3c04c
John Fawcett <jrf0110@gmail.com>
2012-10-24 15:27:09 -0500

    back to prod mode

--- apps/titanium/Goodybag/Resources/lib/core/config.js
+++ apps/titanium/Goodybag/Resources/lib/core/config.js
@@ -16,7 +16,7 @@ gb.config = function (global, fb, storage) {
   
   // debugging modes
   this.debug = true;
-  this.development = true;
+  this.development = false;
   
   // Facebook
   fb.forceDialogAuth = false;
